6ca4f1130abbb318d73756d534aee0f3df97f060
Code Sandbox <hello@codesandbox.io>
2023-07-14 09:06:56 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.501a89c604ce668a484fd802d7a909f5.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff7845b9b88c4765c81140d2dd4427ac",
+    "url": "https://rashika1350.github.io/csb-p7d9ds/index.html"
+  },
+  {
+    "revision": "7441b963c4cd19361e97",
+    "url": "https://rashika1350.github.io/csb-p7d9ds/static/js/2.71074f19.chunk.js"
+  },
+  {
+    "revision": "5cdfb2b8235b120b2568",
+    "url": "https://rashika1350.github.io/csb-p7d9ds/static/js/main.5424b103.chunk.js"
+  },
+  {
+    "revision": "9f7b975d883638c55971",
+    "url": "https://rashika1350.github.io/csb-p7d9ds/static/js/runtime-main.ef783269.js"
+  }
+]);
